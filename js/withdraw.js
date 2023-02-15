afd4235cf2add1138cbac1a4b4d3303f76fcafbf
Joynab1 <joynab.ju.urp@gmail.com>
2023-02-15 12:02:16 +0600

    third-commit

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,9 +3,17 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const newWithdrawAmount = getInputFieldValueById('withdraw-field');
     const previousWithdrawTotal = getTextElementValueById('withdraw-total');
     const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    setElementText('withdraw-total', newWithdrawTotal);
-   const previousBalanceTotal = getTextElementValueById('balance-total');
-   const newBalanceTotal = previousBalanceTotal - newWithdrawTotal;
-   setElementText('balance-total', newBalanceTotal);
-  
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('You Do not have suficient balance');
+       return;
+    }
+    else {
+        setElementText('withdraw-total', newWithdrawTotal);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawTotal;
+    setElementText('balance-total', newBalanceTotal);
+    }
+    
+
 })
